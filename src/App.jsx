0c10df203e6a1967168fda00fedcfdcb276b6371
bruce <bmpandrada@gmail.com>
2025-11-27 17:52:42 +0800

    feat: transfer free template to functional page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BodySection from "./componenst/layouts/bodySection";
+import MainLayout from "./componenst/layouts/main-layouts";
+
+const App = () => {
+  return (
+    <MainLayout>
+      <BodySection />
+    </MainLayout>
+  );
+};
+
+export default App;
